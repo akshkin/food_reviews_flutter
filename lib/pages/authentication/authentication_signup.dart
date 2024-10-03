b9173258b962d3db8ca83cf04f8c9a15c320a45c
akshkin <akshaya.kini12@gmail.com>
2024-10-03 09:55:00 +0200

    Add review entry view, edit, photo zoom and authentication pages

---
+++ lib/pages/authentication/authentication_signup.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UserSignup extends StatefulWidget {
+  const UserSignup({super.key});
+
+  static const String route = "/signup";
+
+  @override
+  State<UserSignup> createState() => _UserSignupState();
+}
+
+class _UserSignupState extends State<UserSignup> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/authentication/forgot_password.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
+  static const String route = "/forgot_password";
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
