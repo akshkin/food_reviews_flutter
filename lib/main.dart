4332f27737e528afbab55e7193ae4394f1f810aa
akshkin <akshaya.kini12@gmail.com>
2024-10-03 20:42:19 +0200

    Initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:food_reviews/firebase_options.dart';
 import 'package:food_reviews/helper/themes.dart';
 import 'package:food_reviews/pages/home.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
