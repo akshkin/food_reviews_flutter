bfac65e4dbc83e6e67320112b8cd9f260caeb040
akshkin <akshaya.kini12@gmail.com>
2024-10-05 16:50:08 +0200

    Create reusable Dialog and MutedText widgets

---
+++ lib/widget/dialogs.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Dialogs {
+  static Future<bool?> showAlertDialog({
+    required BuildContext context,
+    required String title,
+    required String contentDescription,
+    required String noButtonTitle,
+    required String yesButtonTitle,
+  }) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        icon: const Icon(Icons.warning_amber, size: 32),
+        title: Text(title),
+        content: Text(contentDescription),
+        actions: <Widget>[
+          ElevatedButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: Text(noButtonTitle),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: Text(yesButtonTitle),
+          ),
+        ],
+      ),
+    );
+  }
+}
