59104f85d47f8468f369c6deebd9d967556f3509
akshkin <akshaya.kini12@gmail.com>
2024-10-05 16:51:01 +0200

    Create reusable ImageAndMessage and ImageCircleShadow widgets

---
+++ lib/widget/image_circle_shadow.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ImageCircleShadow extends StatelessWidget {
+  const ImageCircleShadow(
+      {super.key, required this.assetImageWithPath, required this.height});
+
+  final String assetImageWithPath;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.square(
+      dimension: height,
+      child: Material(
+        type: MaterialType.circle,
+        color: Colors.transparent,
+        shadowColor: Theme.of(context).indicatorColor,
+        elevation: 8,
+        child: Image(
+          image: AssetImage(assetImageWithPath),
+        ),
+      ),
+    );
+  }
+}
